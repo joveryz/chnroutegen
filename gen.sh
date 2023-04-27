171097cd80a6ab0fbb5a1185ae109e73e844907b
Tongbo Zhang <ztb5129@live.com>
2023-04-27 23:35:12 +0800

    add local networks

--- gen.sh
+++ gen.sh
@@ -2,14 +2,17 @@
 mkdir -p ./tmp
 cd ./tmp
 
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
+wget --no-check-certificate -c -O raw.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
 
 {
 echo "/ip firewall address-list"
 
-for net in $(cat CN.txt) ; do
+for net in $(cat raw.txt) ; do
   echo "add list=fal-chn address=$net comment=chnroute"
 done
+
+echo "add list=fal-chn address=172.16.68.0/24 comment=chnroute"
+echo "add list=fal-chn address=172.16.69.0/24 comment=chnroute"
 
 } > ../chnroute.rsc
 
